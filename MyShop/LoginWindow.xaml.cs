0d193423135c49ceef5d0784515d19af3625cc0a
Hồ Thiên Phúc <1760147@student.hcmus.edu.vn>
2020-10-05 13:30:05 +0700

    Update code behind that is usernameTextBox and passwordPasswordBox

--- MyShop/LoginWindow.xaml.cs
+++ MyShop/LoginWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+﻿using System.Windows;
+using MyShop.ViewModel;
 namespace MyShop
 {
     /// <summary>
@@ -20,9 +7,11 @@ namespace MyShop
     /// </summary>
     public partial class LoginWindow : Window
     {
+        public LoginWindowViewModel ViewModel { get; set; }
         public LoginWindow()
         {
             InitializeComponent();
+            this.DataContext = ViewModel = new LoginWindowViewModel();
         }
     }
 }
---
+++ MyShop/ViewModel/LoginWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Input;
+using System.Windows;
+
+namespace MyShop.ViewModel
+{
+    public class LoginWindowViewModel : BaseViewModel
+    {
+        #region command
+        public ICommand UsernameTextBoxGotFocus { get; set; }
+        public ICommand PasswordPasswordBoxGotFocus { get; set; }
+        public ICommand UsernameTextBoxLostFocus { get; set; }
+        public ICommand PasswordPasswordBoxLostFocus { get; set; }
+        #endregion
+        public LoginWindowViewModel()
+        {
+            UsernameTextBoxGotFocus = new RelayCommand<LoginWindow>
+                (
+                    (p) => { return p == null ? false : true; },
+                    (p) => {
+                        p.usernameLable.Visibility = Visibility.Hidden;
+                    }
+                );
+            UsernameTextBoxLostFocus = new RelayCommand<LoginWindow>
+                (
+                    (p) => { return p == null ? false : true; },
+                    (p) => {
+                        if (p.usernameTextBox.Text.Length == 0)
+                        {
+                            p.usernameLable.Visibility = Visibility.Visible;
+                        }
+                    }
+                );
+            PasswordPasswordBoxGotFocus = new RelayCommand<LoginWindow>
+                (
+                    (p) => { return p == null ? false : true; },
+                    (p) => {
+                        p.passwordLabel.Visibility = Visibility.Hidden;
+                    }
+                );
+            PasswordPasswordBoxLostFocus = new RelayCommand<LoginWindow>
+                (
+                    (p) => { return p == null ? false : true; },
+                    (p) => {
+                        if (p.passwordPasswordBox.Password.Length == 0)
+                        {
+                            p.passwordLabel.Visibility = Visibility.Visible;
+                        }
+                    }
+                );
+        }
+    }
+}
