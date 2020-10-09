9b9b20ab624c778e7b17b31299c2b8f92aa6eb5d
Hồ Thiên Phúc <1760147@student.hcmus.edu.vn>
2020-10-09 11:22:12 +0700

    create UserControl

---
+++ MyShop/UserControl/MasterDataUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyShop.UserControl
+{
+    /// <summary>
+    /// Interaction logic for MasterDataUserControl.xaml
+    /// </summary>
+    public partial class MasterDataUserControl : UserControl
+    {
+        public MasterDataUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
